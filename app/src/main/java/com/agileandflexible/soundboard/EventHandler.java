f304d098025b1d3bd36eb8f47f0fad7e98b3a661
Noweli <patryklewonn@gmail.com>
2020-06-04 19:37:17 +0200

    Refactored and added share method

--- app/src/main/java/com/agileandflexible/soundboard/EventHandler.java
+++ app/src/main/java/com/agileandflexible/soundboard/EventHandler.java
@@ -1,11 +1,15 @@
 package com.agileandflexible.soundboard;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Environment;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.PopupMenu;
+
+import androidx.core.content.FileProvider;
 
 import com.google.android.material.snackbar.Snackbar;
 
@@ -42,34 +46,50 @@ public class EventHandler {
         popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
             @Override
             public boolean onMenuItemClick(MenuItem item) {
+                final String fileName = soundItem.getSoundName() + ".mp3";
+                final File soundFile = new File(GetDirectory(view), fileName);
+
                 switch (item.getItemId()) {
                     case R.id.menu_save_as:
+                       SaveFile(view, soundItem, soundFile);
+                       break;
                     case R.id.menu_share:
-                        final String fileName = soundItem.getSoundName() + ".mp3";
-
-                        File directory = new File(view.getContext().getExternalFilesDir(null) + "/flexibleSoundBoard");
-
-                        if(!directory.mkdir()){
-                            Log.wtf(LOG_TAG, "Directory not created!");
-                        }
-
-                        final File soundFile = new File(directory, fileName);
-
-                        try (InputStream inputStream = view.getContext().getResources().openRawResource(soundItem.getSoundId()); OutputStream outputStream = new FileOutputStream(soundFile)){
-                            byte[] buffer = new byte[1024];
-                            int len;
-                            while((len = inputStream.read(buffer, 0, buffer.length)) != -1){
-                                outputStream.write(buffer, 0, len);
-                            }
-                            Snackbar.make(view, "Saved file to: " + directory.getAbsolutePath(), Snackbar.LENGTH_SHORT);
-                        } catch (IOException e){
-                            Log.e(LOG_TAG, "Failed to save file");
-                        }
+                        SaveFile(view, soundItem, soundFile);
+                        final String AUTHORITY = "com.agileandflexible.fileprovider";
+                        Uri contentUri = FileProvider.getUriForFile(view.getContext(), AUTHORITY, soundFile);
+                        final Intent shareIntent = new Intent(Intent.ACTION_SEND);
+                        shareIntent.putExtra(Intent.EXTRA_STREAM, contentUri);
+                        shareIntent.setType("audio/mp3");
+                        view.getContext().startActivity(Intent.createChooser(shareIntent, "Share sound tile"));
+                        break;
                 }
                 return true;
             }
         });
 
         popupMenu.show();
+    }
+
+    private static void SaveFile(View view, SoundItem soundItem, File soundFile){
+        try (InputStream inputStream = view.getContext().getResources().openRawResource(soundItem.getSoundId()); OutputStream outputStream = new FileOutputStream(soundFile)){
+            byte[] buffer = new byte[1024];
+            int len;
+            while((len = inputStream.read(buffer, 0, buffer.length)) != -1){
+                outputStream.write(buffer, 0, len);
+            }
+            Snackbar.make(view, "Saved file to: " + GetDirectory(view).getAbsolutePath(), Snackbar.LENGTH_SHORT);
+        } catch (IOException e){
+            Log.e(LOG_TAG, "Failed to save file");
+        }
+    }
+
+    private static File GetDirectory(View view){
+        File directory = new File(view.getContext().getExternalFilesDir(null) + "/flexibleSoundBoard");
+
+        if(!directory.mkdir()){
+            Log.wtf(LOG_TAG, "Directory not created!");
+        }
+
+        return directory;
     }
 }
